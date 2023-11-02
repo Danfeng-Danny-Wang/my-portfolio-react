9b93936a7993dec0bb85d27b2a65d49821255233
Danny W <dannywang@utexas.edu>
2023-11-02 02:08:38 -0400

    switch from BrowserRouter to HashRouter to work with gh-pages

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
 import AboutMe from "./pages/AboutMe";
 import Projects from "./pages/Projects";
 import Contact from "./pages/Contact";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route index element={<Navigate replace to="aboutme" />} />
         <Route path="aboutme" element={<AboutMe />} />
         <Route path="projects" element={<Projects />} />
         <Route path="contact" element={<Contact />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
